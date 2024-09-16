82ee96bd35367060926c0537f30b90daa44c8027
Nabin <dhamala.nabin1@gmail.com>
2024-09-16 12:19:43 +0900

    added

--- Mongodb/index.js
+++ Mongodb/index.js
@@ -1,2 +1,15 @@
 const mongoose=require("mongoose")
-mongoose.
+mongoose.connect(
+  "mongodb+srv://NABINDHAMALA:j6F6qFYqqJ3cyNyd@cluster0.0z2j0.mongodb.net/"
+);
+const User = mongoose.model("users", {
+  name: "string",
+  email: "string",
+  password: "string",
+});
+const user = new User({
+  name: "nabin",
+  email: "nabindhamala@yahoo.com",
+  password: "12345",
+});
+user.save();
